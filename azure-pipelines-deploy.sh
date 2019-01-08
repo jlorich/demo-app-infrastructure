3541bd6b8f332d5337a520fb0c751989abbee5f6
Joey Lorich <joseph.lorich@microsoft.com>
2019-01-07 18:20:57 -0700

    Update build pipeline

--- azure-pipelines-deploy.sh
+++ azure-pipelines-deploy.sh
@@ -1,17 +1,17 @@
 # Initialize the local environment
 terraform init \
-    -backend-config="access_key=$(storage-access-key)" \
-    -backend-config="storage_account_name=$(storage-account-name)" \
+    -backend-config="access_key=$STORAGE_ACCESS_KEY" \
+    -backend-config="storage_account_name=$STORAGE_ACCOUTN_NAME" \
     -input=false 
 
 # Select the appropriate workspace
-terraform workspace select $(environment)
+terraform workspace select $ENVIRONMENT
 
 # Generate a Plan for the deployment
 terraform plan \
-    -var environment="$(environment)" \
-    -out=$(environment).tfplan \
+    -var environment="$ENVIRONMENT" \
+    -out=$ENVIRONMENT.tfplan \
     -input=false 
 
 # Apply the environment changes
-terraform apply -input=false $(environment).tfplan
+terraform apply -input=false $ENVIRONMENT.tfplan
