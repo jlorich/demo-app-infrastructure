c5ff2d7a2aed199fc14a40723e1585209d66dc14
Joey Lorich <joseph.lorich@microsoft.com>
2019-01-07 17:56:53 -0700

    move to deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# Initialize the local environment
+terraform init \
+    -backend-config="access_key=$(storage-access-key)" \
+    -backend-config "storage_account_name=$(storage-account-name)" \
+    -input=false 
+
+# Select the appropriate workspace
+terraform workspace select $(environment)
+
+# Generate a Plan for the deployment
+terraform plan \
+    -var environment="$(environment)" \
+    -out=$(environment).tfplan \
+    -input=false 
+
+# Apply the environment changes
+terraform apply -input=false $(environment).tfplan
